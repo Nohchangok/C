dd8a0a2eeaffd01fc873d11a5c7dc514929f9d42
Nohel <ckddhr7560@naver.com>
2020-03-15 15:12:13 +0900

    scu 2-2주차

--- SCU/hello.c
+++ SCU/hello.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
+    
+    //정수(오른쪽정렬)
+    /* printf("%d x %d = %2d\n", 5, 1, 5*1);
+    printf("%d x %d = %d\n", 5, 2, 5*2);
+    printf("%d x %d = %d\n", 5, 3, 5*3);
+    printf("%d x %d = %d\n", 5, 4, 5*4);
+    printf("%d x %d = %d\n", 5, 5, 5*5);
+    */
 
-    printf("hello changok!");
+   //실수(오른쪽정렬)
+   /* printf("%5.2lf x %2d = %5.2lf\n",3.14,1,3.14*1);
+   printf("%5.2lf x %2d = %5.2lf\n",3.14,2,3.14*2);
+   printf("%5.2lf x %2d = %5.2lf\n",3.14,3,3.14*3);
+   printf("%5.2lf x %2d = %5.2lf\n",3.14,10,3.14*10);
+    */
+ 
+   //문자
+   printf("%4s\n", "a");
+   printf("%4s\n", "ab");
+   printf("%4s\n", "abc");
+
     return 0;
+
 }
+
